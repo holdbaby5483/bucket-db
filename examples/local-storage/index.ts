a39f62c5311ab05ec433724255b821929f148889
holdbaby5483 <holdbaby5483@gmail.com>
2026-03-16 18:09:43 +0800

    revert: restore package name to @hold-baby/bucket-db

--- examples/local-storage/index.ts
+++ examples/local-storage/index.ts
@@ -1,5 +1,5 @@
-import { BucketDB, FileSystemAdapter } from 'bucket-db';
-import type { Document } from 'bucket-db';
+import { BucketDB, FileSystemAdapter } from '@hold-baby/bucket-db';
+import type { Document } from '@hold-baby/bucket-db';
 import { existsSync, rmSync } from 'fs';
 
 interface User extends Document {
